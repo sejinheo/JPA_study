f34ebf60b9193461595d4e99bfdb55197e0da03f
허세진 <heosejin222@gmail.com>
2025-07-25 21:24:34 +0900

    준영속 상태

--- src/main/java/jpa_basic/ex1_hello_jpa/hellojpa/JpaMain.java
+++ src/main/java/jpa_basic/ex1_hello_jpa/hellojpa/JpaMain.java
@@ -19,10 +19,13 @@ public class JpaMain {
         try{
             //영속
 
-            Member member = new Member(200L,"member200");
-            em.persist(member);
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
 
-            em.flush();
+            //em.detach(member);
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("===================");
 
