a928be7717a7a381f1a761f846171724e0e7944d
허세진 <heosejin222@gmail.com>
2025-07-25 23:33:14 +0900

    기본 키 매핑

--- src/main/java/jpa_basic/ex1_hello_jpa/hellojpa/Member.java
+++ src/main/java/jpa_basic/ex1_hello_jpa/hellojpa/Member.java
@@ -8,27 +8,11 @@ import java.util.Date;
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
    @Column(name = "name") // 컬럼 매핑
    private String username;
-
-   private Integer age;
-
-   @Enumerated(EnumType.STRING) // enum 타입 매핑
-   private RoleType roleType;
-
-   @Temporal(TemporalType.TIMESTAMP) // 날짜 타입 매핑
-   private Date createDate;
-
-   @Temporal(TemporalType.TIMESTAMP)
-   private Date lastModifiedDate;
-
-   @Lob // BLOB, CLOB 매핑
-   private String description;
-
-   @Transient // 메모리에서만 쓸 때
-   private int temp;
 
     public Member() {
     }
